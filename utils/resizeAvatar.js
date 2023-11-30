4468efd0a3d11a08a7fd9670a20888ad94ecbbd2
groguls <groguls@gmail.com>
2023-11-30 23:52:20 +0100

    avatar resize method updated

--- utils/resizeAvatar.js
+++ utils/resizeAvatar.js
@@ -2,7 +2,11 @@ const Jimp = require("jimp");
 
 const resizeAvatar = async (pathToAvatar) => {
   const avatar = await Jimp.read(pathToAvatar);
-  await avatar.resize(250, Jimp.AUTO);
+  await avatar.image.contain(
+    250,
+    250,
+    Jimp.HORIZONTAL_ALIGN_CENTER | Jimp.VERTICAL_ALIGN_MIDDLE
+  );
   await avatar.writeAsync(pathToAvatar);
   return null;
 };
